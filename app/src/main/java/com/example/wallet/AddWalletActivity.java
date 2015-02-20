2b3a21d324ff3dd4ca481ec2e65aeac98ed1cc3e
Alexey Zakharov <alexey.v.zaharov@gmail.com>
2015-02-19 23:44:58 -0800

    show error code

--- app/src/main/java/com/example/wallet/AddWalletActivity.java
+++ app/src/main/java/com/example/wallet/AddWalletActivity.java
@@ -27,9 +27,12 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.wallet.MaskedWallet;
 import com.google.android.gms.wallet.MaskedWalletRequest;
 import com.google.android.gms.wallet.WalletConstants;
+import com.google.android.gms.wallet.fragment.BuyButtonAppearance;
 import com.google.android.gms.wallet.fragment.BuyButtonText;
 import com.google.android.gms.wallet.fragment.Dimension;
 import com.google.android.gms.wallet.fragment.SupportWalletFragment;
@@ -44,14 +47,14 @@ import wallet.example.com.walletexample.R;
 public class AddWalletActivity extends Activity {
     private static final String TAG = "AddWalletActivity";
 
-    private static final int REQUEST_CODE_MASKED_WALLET = 1;
+    private static final int REQUEST_CODE_MASKED_WALLET = 1234;
     private WalletFragment mWalletFragment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_wallet);
-        
+
         createAndAddWalletFragment();
     }
 
@@ -65,12 +68,59 @@ public class AddWalletActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Log.d(TAG, "onActivityResult");
+        int errorCode = -1;
+        if (data != null) {
+            errorCode = data.getIntExtra(WalletConstants.EXTRA_ERROR_CODE, -1);
+        }
+        switch (requestCode) {
+            case REQUEST_CODE_MASKED_WALLET:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        MaskedWallet maskedWallet =
+                                data.getParcelableExtra(WalletConstants.EXTRA_MASKED_WALLET);
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                    default:
+                        handleError(errorCode);
+                        break;
+                }
+                break;
+            case WalletConstants.RESULT_ERROR:
+                handleError(errorCode);
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
+    }
+
+    protected void handleError(int errorCode) {
+        switch (errorCode) {
+            case WalletConstants.ERROR_CODE_SPENDING_LIMIT_EXCEEDED:
+                Toast.makeText(this, getString(R.string.spending_limit_exceeded, errorCode),
+                        Toast.LENGTH_LONG).show();
+                break;
+            case WalletConstants.ERROR_CODE_INVALID_PARAMETERS:
+            case WalletConstants.ERROR_CODE_AUTHENTICATION_FAILURE:
+            case WalletConstants.ERROR_CODE_BUYER_ACCOUNT_ERROR:
+            case WalletConstants.ERROR_CODE_MERCHANT_ACCOUNT_ERROR:
+            case WalletConstants.ERROR_CODE_SERVICE_UNAVAILABLE:
+            case WalletConstants.ERROR_CODE_UNSUPPORTED_API_VERSION:
+            case WalletConstants.ERROR_CODE_UNKNOWN:
+            default:
+                // unrecoverable error
+                String errorMessage = getString(R.string.google_wallet_unavailable) + "\n" +
+                        getString(R.string.error_code, errorCode);
+                Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 
     private void createAndAddWalletFragment() {
         WalletFragmentStyle walletFragmentStyle = new WalletFragmentStyle()
-                .setBuyButtonText(BuyButtonText.BUY_NOW)
+                .setBuyButtonAppearance(BuyButtonAppearance.MONOCHROME)
+                .setBuyButtonText(BuyButtonText.BUY_WITH_GOOGLE)
                 .setBuyButtonWidth(Dimension.MATCH_PARENT);
 
         WalletFragmentOptions walletFragmentOptions = WalletFragmentOptions.newBuilder()
@@ -100,10 +150,10 @@ public class AddWalletActivity extends Activity {
     private static MaskedWalletRequest createMaskedWalletRequest() {
 
         return MaskedWalletRequest.newBuilder()
-                .setMerchantName(Constants.MERCHANT_NAME)
+                .setMerchantName("Google I/O Codelab")
                 .setPhoneNumberRequired(true)
                 .setShippingAddressRequired(true)
-                .setCurrencyCode(Constants.CURRENCY_CODE_USD)
+                .setCurrencyCode("USD")
                 .setIsBillingAgreement(true)
                 .build();
     }
